ef2f37ba430cff80721cb36637c8c40d7df2bd1e
Mateus Marques <mateusmarques200@gmail.com>
2023-06-22 10:00:32 -0300

    FEAT: adding props to ThoughtCard

--- src/components/ThoughtCard/index.tsx
+++ src/components/ThoughtCard/index.tsx
@@ -1,26 +1,25 @@
+import { Thought } from '../../redux/reducers/types'
 import * as S from './styled'
-const ThoughtCard = () => {
+
+const ThoughtCard = ({ id, title, content, created_datetime, username }: Thought) => {
   return (
     <S.Container>
       <S.CardHeader>
         <S.CardHeaderTitle>
-          My first post at CodeLeapNetwork
+          {title}
         </S.CardHeaderTitle>
       </S.CardHeader>
       <S.CardContentContainer>
         <S.CardContentHeader>
           <S.CardContentUser>
-            Victor
+            {username}
           </S.CardContentUser>
           <S.CardContentTime>
-            25 minutes ago
+            {created_datetime.toISOString()}
           </S.CardContentTime>
         </S.CardContentHeader>
         <S.CardContentText>
-          Curabitur suscipit suscipit tellus. Phasellus consectetuer vestibulum elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas egestas arcu quis ligula mattis placerat. Duis vel nibh at velit scelerisque
-          
-          suscipit. Duis lobortis massa imperdiet quam. Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis lacus. Fusce a quam. Nullam vel sem. Nullam cursus lacinia erat.
-
+          {content}
         </S.CardContentText>
       </S.CardContentContainer>
     </S.Container>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,7 +8,14 @@ const Home = () => {
     <Container>
       <S.ContentContainer>
         <HomeContent />
-        <ThoughtCard />
+        <ThoughtCard 
+          title="My first post at CodeLeap Network"
+          content="Curabitur suscipit suscipit tellus. Phasellus consectetuer vestibulum elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas egestas arcu quis ligula mattis placerat. Duis vel nibh at velit scelerisque suscipit.
+          Duis lobortis massa imperdiet quam. Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis lacus. Fusce a quam. Nullam vel sem. Nullam cursus lacinia erat."
+          created_datetime={new Date}
+          username="Mateus"
+          id={123}
+        />
       </S.ContentContainer>
     </Container>
   )
